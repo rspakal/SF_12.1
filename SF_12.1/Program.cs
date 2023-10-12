b55ed949f7b8852291ffb76e09bd79c55346f9df
rspakal <gibsrom@gmail.com>
2023-10-12 16:29:26 +0300

    Add project files.

---
+++ SF_12.1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SF_12._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            List<User> _users = new List<User>()
+            {
+                new User("pavel", "Pavel", true),
+                new User("ivan","Ivan", false)
+            };
+            foreach (var user in _users) 
+            {
+                Console.WriteLine(user.Name + ", hello!");
+                if (!user.IsPremium)
+                    ShowAds();
+            }
+            Console.ReadKey();
+        }
+        static void ShowAds()
+        {
+            Console.WriteLine("Посетите наш новый сайт с бесплатными играми free.games.for.a.fool.com");
+            // Остановка на 1 с
+            Thread.Sleep(1000);
+
+            Console.WriteLine("Купите подписку на МыКомбо и слушайте музыку везде и всегда.");
+            // Остановка на 2 с
+            Thread.Sleep(2000);
+
+            Console.WriteLine("Оформите премиум-подписку на наш сервис, чтобы не видеть рекламу.");
+            // Остановка на 3 с
+            Thread.Sleep(3000);
+        }
+    }
+}
---
+++ SF_12.1/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SF_12._1
+{
+    internal class User
+    {
+        public string Login { get; set; }
+        public string Name { get; set; }
+        public bool IsPremium { get; set; }
+        public User(string login, string name, bool isPremium) 
+        {
+            Login = login;
+            Name = name;
+            IsPremium = isPremium;
+        }
+    }
+}
